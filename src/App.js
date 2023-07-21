64ac5d52d0ad67b9aff93d7289f7e72162cc8276
frankothe196 <kipkalisbartoo@gmail.com>
2023-07-21 11:41:30 +0300

    Step 1: complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
 
   // PseudoCode
   // We need to make an item draggable, while maintaining a great good user experience
-  // Step 1: Handle mouse hold
-  // Step 2: Mark/Select the item as dragged
-  // Step 3: Add a place holder/empty space for the item and make a copy of the item.
-  // Step 4: Make the item hover with the mouse movement
-  // Step 5: if mouse leaves boundary cancel the whole operation
-  // Step 6: On mouse release on a certain element, place the element to the left of that element, handle any shifting where necessary
+  // Step 1: Handle mouse hold, Mark/Select the item as dragged if held for 1 second
+  const [selected,setSelected] = useState()
+  const timer = useRef()
+
+
+  
+  const handleMouseDown = (e)=>{
+    // On mouse down start a 1 second timer
+    timer.current = setTimeout(() => {
+        console.log(`Selected item ${e.target.id}`)
+        setSelected(e.target.id)
+    }, 1000
+    );
+  }
+
+  const handleMouseUp = ()=>{
+    console.log('mouse up')
+    // On mouse up clear Interval
+    clearTimeout(timer.current)
+  }
+
+  // Step 2: Add a place holder/empty space for the item and make a copy of the item (or its data).
+  // Step 3: Make the item hover with the mouse movement
+  // Step 4: if mouse leaves boundary cancel the whole operation
+  // Step 5: On mouse release on a certain element, place the element to the left of that element, handle any shifting where necessary
   
   // NB: Is it better to actually move the whole element or just move its data?
 
@@ -24,7 +44,7 @@ function App() {
       </header>
       <main>
         <section className="container">
-          <div><span>Item 1</span></div>
+          <div id={1} onMouseDown={(e)=>handleMouseDown(e)} onMouseUp={()=>{handleMouseUp()}}><span>Item 1</span></div>
           <div><span>Item 2</span></div>
           <div><span>Item 3</span></div>
           <div><span>Item 4</span></div>
